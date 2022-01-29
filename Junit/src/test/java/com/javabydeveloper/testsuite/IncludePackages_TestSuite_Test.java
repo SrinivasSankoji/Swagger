95bf92ca4dcffe652e4a58cb07b0adfbc87545c7
srinivassankoji <srinivaschary.chary@gmail.com>
2022-01-30 01:29:40 +0530

    SOAP Web Service

--- Junit/src/test/java/com/javabydeveloper/testsuite/IncludePackages_TestSuite_Test.java
+++ Junit/src/test/java/com/javabydeveloper/testsuite/IncludePackages_TestSuite_Test.java
@@ -2,12 +2,9 @@ package com.javabydeveloper.testsuite;
 
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.SuiteDisplayName;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
-@SuiteDisplayName("Run Junit5 Test Cases in Junit4")
 @SelectPackages("com.javabydeveloper.payment")
-//@SelectClasses(PaymentTest.class)
 public class IncludePackages_TestSuite_Test {
 }
---
+++ spring-boot-soap-service/src/test/java/com/example/howtodoinjava/springbootsoapservice/SpringBootSoapServiceApplicationTests.java
@@ -0,0 +1,8 @@
+package com.example.howtodoinjava.springbootsoapservice;
+
+public class SpringBootSoapServiceApplicationTests {
+
+	public void contextLoads() {
+	}
+
+}
